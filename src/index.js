59cd7bdf8a1a41b46471c444eddec0a517f2747e
corzofederico <94192972+corzofederico@users.noreply.github.com>
2022-02-21 12:53:42 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from 'App';
+import App from './App';
+import { unregister } from 'serviceWorker';
 
 ReactDOM.render(
-    <App />,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
+unregister();
