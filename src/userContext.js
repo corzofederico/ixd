6f9aa8b26854df58b40594a03a4887bcec430c4d
corzofederico <94192972+corzofederico@users.noreply.github.com>
2022-02-21 13:21:23 -0300

    Update userContext.js

--- src/userContext.js
+++ src/userContext.js
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 const Context = React.createContext({})
 
 export function UserContextProvider ({children}) {
-  if(Boolean(window.localStorage.user)===false){ window.localStorage.user={};}
+  if(Boolean(window.localStorage.user)===false){ window.localStorage.user="";}
   const [user, setUser] = useState(
     () => window.localStorage.user
   )
