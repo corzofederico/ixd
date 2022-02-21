0dfb744c90f49221a1d5e088731f4a15b61817e0
Federico <Federico@HP-Federico.corzofederico>
2022-02-21 14:29:03 -0300

    upload without bugs

--- src/components/log/index.js
+++ src/components/log/index.js
@@ -7,7 +7,7 @@ export default function Log ({type}){
     const {user, toogleMenuState} = useContext(Context)
     var btn="Regístrate"
     return <>{Boolean(user)===false&&
-        <Link to='/log/check' onClick={toogleMenuState}><div align="center" className={"sign"+(type==="normal"?" log":"")}>{type==="lmenu"?btn:<>
+        <Link to='/log/check' onClick={(type!=="normal")&&toogleMenuState}><div align="center" className={"sign"+(type==="normal"?" log":"")}>{type==="lmenu"?btn:<>
             <b>Descubre IxD Bahía desde tu casa</b>
             <img src="/singin.svg" alt="Illustration for signin"/>
             <button className="bg-blue big">{btn}</button>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
-import App from 'App';
+import { Route, Switch } from "wouter";
+import 'App.css';
+import { UserContextProvider } from "userContext";
+
+import Menu from 'components/Menu';
+import Comunidad from 'pages/Comunidad';
+import Log from 'pages/Log';
+import Profile from 'pages/Profile';
+
+const HomePage = React.lazy(() => import("pages/Home"));
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+    <UserContextProvider>
+        <Menu />
+        <div id="cont">
+          <Suspense fallback={<b>Loading...</b>}>
+            <Switch>
+              <Route component={HomePage} path="/" />
+              <Route component={Log} path="/log/:type" />
+              <Route component={Comunidad} path="/comunidad/:search?" />
+              <Route component={Profile} path="/user/:usr" />
+              <Route component={Profile} path="/project/:proj" />
+              <Route>404, Not Found!</Route>
+            </Switch>
+          </Suspense>
+        </div>
+    </UserContextProvider>
+,document.body)
